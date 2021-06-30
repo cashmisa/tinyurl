5df62540a0ff57315385a444e2b4ac8c7e60051f
cashmisa <cashmisa@hotmail.com>
2021-06-30 23:19:50 +0800

    remove redundant

--- tinyurl/src/main/java/dev/xiaowen/demo/config/WebMvcConfiguration.java
+++ tinyurl/src/main/java/dev/xiaowen/demo/config/WebMvcConfiguration.java
@@ -2,7 +2,6 @@ package dev.xiaowen.demo.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,8 +12,5 @@ public class WebMvcConfiguration implements WebMvcConfigurer
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index").setViewName("index");
-        registry.addViewController("/error").setViewName("error");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
